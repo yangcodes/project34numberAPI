b3b2b7218ecd825e15c6cc3652f96eb714faf6f6
Jenny Lin <misslinyang91@gmail.com>
2021-09-03 10:54:35 +1000

    fetched the number facts

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 // http://numbersapi.com/
+const factBtn = document.querySelector("button");
+const factInput = document.querySelector("input");
+const factPara = document.querySelector("p");
+
+factBtn.addEventListener("click", () => {
+  const number = factInput.value;
+  const numberFact = parseInt(number);
+
+  fetchFact(numberFact);
+});
+
+async function fetchFact(number) {
+  const factURL = "http://numbersapi.com/";
+  //const proxyURL = "http://cors-anywhere.herokuapp.com/";
+
+  const response = await fetch(`${factURL}${number}`, {
+    method: "GET",
+    headers: {
+      "x-requested-with": "text/plain",
+    },
+  });
+
+  const responseData = await response.text();
+  console.log(responseData);
+
+  factPara.innerText = responseData;
+}
